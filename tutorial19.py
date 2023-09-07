432306524e3ad0f9072e449400f72819d4a0dc57
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-09-07 23:29:05 +0530

    Add files via upload

---
+++ tutorial19.py
@@ -0,0 +1,11 @@
+# while loops
+# condition controlled loop - while loop
+# while expression:
+#     statement(s)
+
+# code
+
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello shakyo")
